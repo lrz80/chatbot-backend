1aa277765b9f1df2c173762eb11497b8e85f3c0f
Luis <luisrz09@gmail.com>
2025-04-12 18:17:19 -0400

    fix: usar PORT de Railway

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use('/auth', authRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
