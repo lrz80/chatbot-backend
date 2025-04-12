c1f57be8c5281a4e268c525bf20d1ebd9608f11d
Luis <luisrz09@gmail.com>
2025-04-12 19:02:50 -0400

    refactor: eliminar uso de archivo json, usar FIREBASE_CONFIG

--- src/firebase/admin.js
+++ src/firebase/admin.js
@@ -1,13 +1,13 @@
-import { readFileSync } from 'fs';
 import { initializeApp, cert, getApps } from 'firebase-admin/app';
 
 export const initFirebase = () => {
   if (getApps().length === 0) {
-    const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
-
+    const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG);
+    
     initializeApp({
-      credential: cert(serviceAccount),
+      credential: cert(firebaseConfig)
     });
+
+    console.log('✅ Firebase inicializado con FIREBASE_CONFIG');
   }
 };
-
