77045855af808651767360b3d8c3807603398b3b
Luis <luisrz09@gmail.com>
2025-04-15 08:51:56 -0400

    build: trigger deploy for updated CORS config

--- src/app.ts
+++ src/app.ts
@@ -24,6 +24,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   }
+  console.log("🟢 CORS configurado correctamente");
 
   if (req.method === 'OPTIONS') {
     return res.sendStatus(204);
