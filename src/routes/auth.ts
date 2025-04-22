675bf41bdf650312d34eaa174151b65709d82d2f
Luis <luisrz09@gmail.com>
2025-04-22 15:43:57 -0400

    🚀 Fix: middleware auth obtiene tenant_id desde DB

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -138,7 +138,6 @@ router.post('/login', async (req: Request, res: Response) => {
       httpOnly: true,
       secure: true,
       sameSite: 'none',
-      partitioned: true, // 👈 esto es lo único nuevo
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
