586d6db0d117e36341629a5e8d7751381826c2e9
Luis <luisrz09@gmail.com>
2025-05-22 18:38:03 -0400

    🔥 Eliminar soporte

--- src/routes/stripe/checkout.ts
+++ src/routes/stripe/checkout.ts
@@ -17,7 +17,7 @@ router.post('/checkout', async (req, res) => {
   }
 
   const stripe = new Stripe(STRIPE_SECRET_KEY, {
-    apiVersion: '2025-03-31.basil',
+    apiVersion: '2022-11-15',
   });
 
   const token = req.cookies.token;
