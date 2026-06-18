93ffe56fee26ec6aaa368b41e7fe54550f1f81f3
Luis <luisrz09@gmail.com>
2026-06-18 08:47:24 -0400

    📂 forzar inclusion de uploads

--- src/lib/voice/realtime/openaiRealtimeBridge.ts
+++ src/lib/voice/realtime/openaiRealtimeBridge.ts
@@ -1116,36 +1116,6 @@ export async function createOpenAiRealtimeBridge({
         lastUserTranscriptSeq,
       });
 
-      const bookingTurnStatusAfterResponseDone = clean(
-        (realtimeState as any).bookingTurnStatus
-      );
-
-      const pendingBookingStepKeyAfterResponseDone = clean(
-        (realtimeState as any).pendingBookingStepKey
-      );
-
-      if (
-        !isCancelledExactPromptResponse &&
-        isBookingAssistantPromptResponse &&
-        pendingBookingStepKeyAfterResponseDone &&
-        bookingTurnStatusAfterResponseDone === "waiting_assistant_prompt"
-      ) {
-        realtimeState = {
-          ...realtimeState,
-          bookingTurnStatus: "waiting_user_answer",
-        } as CallState;
-
-        console.log("[VOICE_REALTIME][BOOKING_TURN_OPENED_AFTER_RESPONSE_DONE]", {
-          callSid,
-          responseId: responseStateBeforeDone.activeResponseId,
-          completedResponseSource,
-          pendingBookingStepKey: pendingBookingStepKeyAfterResponseDone,
-          previousBookingTurnStatus: bookingTurnStatusAfterResponseDone,
-          nextBookingTurnStatus: "waiting_user_answer",
-          lastUserTranscriptSeq,
-        });
-      }
-
       responseController.markResponseDone({
         lastUserTranscriptSeq,
       });
