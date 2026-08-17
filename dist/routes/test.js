cacc0bb5692a432010ecf0a79cc07081ae528ca0
Luis <luisrz09@gmail.com>
2026-08-17 17:00:33 -0400

    Stop tracking dist

--- dist/routes/test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router.post('/', (req, res) => {
-    console.log("✅ Recibido POST en /api/test");
-    res.send("🟢 Ruta POST /api/test activa");
-});
-exports.default = router;
